a03092066fa5cc2440cc59d9cda4c783d538edca
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-08-18 20:55:35 -0300

    created hardhat structure, deployer & nft contract

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    const PlatziPunks = await ethers.getContractFactory("PlatziPunks");
+    const deployed = await PlatziPunks.deploy();
+  
+    console.log("Contract address:", deployed.address, `Etherscan URL: https://rinkeby.etherscan.io/address/${deployed.address}`);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
